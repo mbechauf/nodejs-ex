235cbfc443e403f59c1c79edb64d58d3f0bac082
Michael Bechauf <bechauf.michael@gmail.com>
2017-03-23 10:33:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/mbechauf2', function (req, res) {
+app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
